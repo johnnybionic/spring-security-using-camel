7ddab2327c62daa8bbf85099692a72d083d8a23d
johnnybionic <jdavddlo@gmail.com>
2016-07-14 09:16:57 +0100

    Refactored for clarity, added profiles

---
+++ src/main/java/org/johnnybionic/custom/simple/AlternateSimpleAuthenticationProvider.java
@@ -0,0 +1,33 @@
+package org.johnnybionic.custom;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Profile("secondary")
+@Component("camelRouteAuthenticationProvider")
+public class AlternateSimpleAuthenticationProvider implements AuthenticationProvider {
+
+
+	private static final String SECONDARY_USER = "second";
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		
+		if (!SECONDARY_USER.equals(authentication.getName())) {
+			throw new BadCredentialsException("Invalid credentials");
+		}
+		
+		return authentication;
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+}
